bfbf8898a780078d9017d3b6b763830ad835db6d
yami <ravirrajpatil@gmail.com>
2025-10-05 00:54:40 +0530

    Add user and video models with mongoose schema and bcrypt integration

---
+++ src/models/Video.models.ts
@@ -0,0 +1,50 @@
+import mongoose , {model , models , Schema} from "mongoose"
+
+
+export const VIDEO_DIMENTIONS = {
+    height:1920,
+    width:1080
+}as const
+
+export interface IVideo{
+    userId: mongoose.Types.ObjectId;
+    videoUrl:string;
+    description:string;
+    thumbnailUrl:string;
+    transform?:{
+        width:number;
+        height:number;
+    }
+}
+
+
+const videoSchema = new Schema<IVideo>({
+    userId:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    videoUrl:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true,
+        trim:true,
+        maxlength:2200
+    },
+    thumbnailUrl:{
+        type:String,
+        required:true
+    },
+    transform:{
+        width:Number,
+        height:Number
+    }
+},{timestamps:true})        
+
+
+const Video = models?.Video || model<IVideo>("Video", videoSchema)
+
+export default Video
